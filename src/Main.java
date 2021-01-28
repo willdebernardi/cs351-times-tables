2bd2af545ffa21544c70b918316602cf37871485
Will DeBernardi <wdebernardi@unm.edu>
2021-01-27 23:51:47 -0700

    Add line creation that doesn't work at all

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,22 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Point2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.effect.Light;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Shape;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 
 public class Main extends Application {
+    ArrayList<Double> xCoord = new ArrayList<>();
+    ArrayList<Double> yCoord = new ArrayList<>();
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        // Creates a ring by subtracting smaller circle from outside circle
-//        Circle outsideCircle = new Circle(100,100,100);
-//        Circle insideCircle = new  Circle(100,100,95);
-//        Shape finalCircle = Shape.subtract(outsideCircle, insideCircle);
         AnchorPane root = new AnchorPane();
         Scene scene = new Scene(root, 300, 300, Color.LIGHTGREY);
 
@@ -40,9 +33,20 @@ public class Main extends Application {
         for (int i = 0; i < N; i++) {
             Circle pt = new Circle(150.0f + 100 * Math.cos(Math.PI * 2 * i / N),
                     150.0f + 100 * Math.sin(Math.PI * 2 * i / N), 3.00f);
-
+            xCoord.add(pt.getCenterX());
+            yCoord.add(pt.getCenterY());
             pt.setFill(Color.BLACK);
             root.getChildren().add(pt);
+        }
+//        String xCoordinates = xCoord.toString();
+//        String yCoordinates = yCoord.toString();
+//        System.out.println("x: " + xCoordinates);
+//        System.out.println("y: " + yCoordinates);
+
+        int multiple = 2;
+        for(int i = 0;  i < 10; i++) {
+            Line line =  new Line(xCoord.get(i), yCoord.get(i),xCoord.get(i*multiple), yCoord.get(i*multiple));
+            root.getChildren().add(line);
         }
 
         primaryStage.setScene(scene);
